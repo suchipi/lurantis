2b2418c6a1302386b2043f4ba480754ea6953df0
Lily Scott <git@suchipi.tv>
2022-02-03 23:26:47 -0700

    Fix rootdir in bundle

--- kame/resolver.js
+++ kame/resolver.js
@@ -1,8 +1,16 @@
+const path = require("path");
 const kame = require("kame");
 
 exports.interfaceVersion = 2;
 
 exports.resolve = function resolve(id, fromFilePath) {
   if (id === "first-base") return "external:first-base";
-  return kame.defaultResolver(id, fromFilePath);
+
+  const result = kame.defaultResolver(id, fromFilePath);
+
+  if (result === path.resolve(__dirname, "..", "rootdir.js")) {
+    return "external:../rootdir.js";
+  }
+
+  return result;
 };
