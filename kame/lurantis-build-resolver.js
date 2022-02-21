6ec1ed72c96f100d76f0ae841efef1845b8ae553
Lily Scott <me@suchipi.com>
2022-02-21 00:10:20 -0700

    kame build resolver fixes
    - ignore optional dep acorn of terser
    - fix default resolver call for kame 0.7.0 change

--- kame/lurantis-build-resolver.js
+++ kame/lurantis-build-resolver.js
@@ -5,8 +5,10 @@ const kame = require("kame");
 
 exports.resolve = function resolve(id, fromFilePath) {
   if (id === "first-base") return "external:first-base";
+  // optional dep of terser; not used by us
+  if (id === "acorn") return "external:acorn";
 
-  const result = kame.defaultResolver(id, fromFilePath);
+  const result = kame.defaultResolver.resolve(id, fromFilePath);
 
   if (result === path.resolve(__dirname, "..", "rootdir.js")) {
     return "external:../rootdir.js";
