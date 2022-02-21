7abe9e22685a71482742f18b0be9848cb1ce4627
Lily Scott <me@suchipi.com>
2022-02-21 00:22:15 -0700

    work around eval require in pretty print error

--- kame/build-config.js
+++ kame/build-config.js
@@ -1,7 +1,14 @@
-// this is the resolver used to build lurantis itself, not the resolver used in lurantis for bundling stuff.
-
+// this stuff is used to build lurantis itself, not used in lurantis for bundling stuff.
 const path = require("path");
 const kame = require("kame");
+
+exports.load = function load(filepath) {
+  const code = kame.defaultLoader.load(filepath);
+
+  // pretty-print-error obfuscates this kleur require to avoid it ending up
+  // in browser bundles, but we want it in our bundle, which runs in node.
+  return code.replace(`eval("require")("kleur")`, `require("kleur")`);
+};
 
 exports.resolve = function resolve(id, fromFilePath) {
   if (id === "first-base") return "external:first-base";
