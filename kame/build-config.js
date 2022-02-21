edc7dcac5b549ce6a7c3d7f5f5e3bb0b1c0780c5
Lily Scott <me@suchipi.com>
2022-02-21 00:26:40 -0700

    ugh, please pretty-print-error, you're making me look bad

--- kame/build-config.js
+++ kame/build-config.js
@@ -2,16 +2,10 @@
 const path = require("path");
 const kame = require("kame");
 
-exports.load = function load(filepath) {
-  const code = kame.defaultLoader.load(filepath);
-
-  // pretty-print-error obfuscates this kleur require to avoid it ending up
-  // in browser bundles, but we want it in our bundle, which runs in node.
-  return code.replace(`eval("require")("kleur")`, `require("kleur")`);
-};
-
 exports.resolve = function resolve(id, fromFilePath) {
   if (id === "first-base") return "external:first-base";
+  if (id === "pretty-print-error") return "external:pretty-print-error";
+
   // optional dep of terser; not used by us
   if (id === "acorn") return "external:acorn";
 
